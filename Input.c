108e6110a1acaac11018c7cab156f2af803a3eb2
TobiasBlomqvist0 <90187968+TobiasBlomqvist0@users.noreply.github.com>
2021-09-06 13:41:04 +0300

    Add files via upload

---
+++ Input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char Hello[100];
+    
+    
+    printf("Enter your first name: ");
+    scanf("%s",Hello);
+    printf("Hello %s\n", Hello);
+
+    return 0;
+}
